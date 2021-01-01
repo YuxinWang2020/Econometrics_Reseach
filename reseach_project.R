a59522a7dc756f778880f61a41ba498617885fd7
YuxinWang2020 <yuxinwang9712@gmail.com>
2021-01-01 23:41:41 +0800

    V2

--- reseach_project.R
+++ reseach_project.R
@@ -1,18 +1,22 @@
 rm(list = ls())
-library(MASS)
+if (!require("MASS")) install.packages("MASS")
 set.seed(100)
 
-# K = 2
-# T_ = 3
-# N = 4
-# range_x = c(0,1)
-# beta_i = c(1,2) # 长度为K
-# alpha_i = 1
-# mu_u_i = c(0,0,0) # 长度为T_, u的期望
-# sigma_u_i = diag(c(1,1,1)) # u的协方差
 
 ##### DGP #####
 DGP <- function(K, T_, N, range_x, beta_i, alpha_i, mu_u_i, sigma_u_i){
+  if(length(beta_i) != K){
+    warning("length of beta_i != K")
+    return(c())
+  }
+  if(length(mu_u_i) != T_){
+    warning("length of mu_u_i != T")
+    return(c())
+  }
+  if(length(diag(sigma_u_i)) != T_){
+    warning("dims of sigma_u_i != (T,T)")
+    return(c())
+  }
   X <- lapply(as.list(1:N), function(i) array(runif(K*T_,range_x[1],range_x[2]), dim = c(T_, K)))
   U <- lapply(as.list(1:N), function(i) mvrnorm(n = 1, mu = mu_u_i, Sigma = sigma_u_i))
   Y <- mapply(function(X_i, u_i) alpha_i + X_i %*% beta_i + u_i, X, U, SIMPLIFY=F)
@@ -30,10 +34,11 @@ DGP <- function(K, T_, N, range_x, beta_i, alpha_i, mu_u_i, sigma_u_i){
 }
 
 OLS_FE <- function(X, Y){
-  # if(length(dim(X_i)) != 2){
-  #   warning("X_i is not 2d array")
-  #   return(c())
-  # }
+  if(length(dim(X[[1]])) != 2){
+    warning("X_i is not 2d array")
+    return(c())
+  }
+  
   N = length(X)
   K = dim(X[[1]])[2]
   T_ = dim(X[[1]])[1]
@@ -51,12 +56,13 @@ OLS_FE <- function(X, Y){
   return(beta_hat_fe)
 }
 
-dgp = DGP(K = 2,
+dgp = DGP(K = 3,
           T_ = 3,
-          N = 4,
+          N = 400,
           range_x = c(0,1),
-          beta_i = c(1,2),
+          beta_i = c(1,2,3),
           alpha_i = 1,
           mu_u_i = c(0,0,0),
           sigma_u_i = diag(c(1,1,1)))
 OLS_FE(dgp$X, dgp$Y)
+
