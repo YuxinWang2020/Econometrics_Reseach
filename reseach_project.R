dbee2be132f4b85f79ac730039725fece4c377e3
YuxinWang2020 <wyx9712@foxmail.com>
2021-01-01 23:58:23 +0800

    V5

--- reseach_project.R
+++ reseach_project.R
@@ -5,23 +5,21 @@ if (!require("akima")) install.packages("akima")
 if (!require("plotly")) install.packages("plotly")
 if (!require("reshape2")) install.packages("reshape2")
 # if (!require("cowplot")) install.packages("cowplot")
+if (!require("plm")) install.packages("plm")
 set.seed(100)
 
 
 ##### DGP #####
-DGP <- function(K, T_, N, range_x, beta_i, alpha_i, mu_u_i, sigma_u_i){
+DGP <- function(K, T_, N, beta_i){
   if(length(beta_i) != K){
     warning("length of beta_i != K")
     return(c())
   }
-  if(length(mu_u_i) != T_){
-    warning("length of mu_u_i != T")
-    return(c())
-  }
-  if(length(diag(sigma_u_i)) != T_){
-    warning("dims of sigma_u_i != (T,T)")
-    return(c())
-  }
+  range_x = c(0,1)
+  alpha_i = 1
+  mu_u_i = rep(0, T_)
+  sigma_u_i = diag(rep(1, T_))
+  
   X <- lapply(as.list(1:N), function(i) array(runif(K*T_,range_x[1],range_x[2]), dim = c(T_, K)))
   U <- lapply(as.list(1:N), function(i) mvrnorm(n = 1, mu = mu_u_i, Sigma = sigma_u_i))
   Y <- mapply(function(X_i, u_i) alpha_i + X_i %*% beta_i + u_i, X, U, SIMPLIFY=F)
@@ -35,7 +33,51 @@ DGP <- function(K, T_, N, range_x, beta_i, alpha_i, mu_u_i, sigma_u_i){
   #     X[[i]] = X_i
   #     Y[[i]] = Y_i
   # }
-  return(list(X = X, Y = Y))
+  
+  df <- data.frame(i = rep(c(1:N), each = T_),
+                   t = rep(c(1:T_), times = N),
+                   y_it = NA) %>% cbind(matrix(NA,nrow=N*T_,ncol=K))
+  colnames(df)[4:(3+K)] <- paste("x",c(1:K), sep = "")
+
+  for(i in 1:N){
+    for(t in 1:T_){
+      df[(i-1)*T_ + t, 4:(3+K)] <- X[[i]][t,]
+      df$y_it[(i-1)*T_ + t] <- Y[[i]][t]
+    }
+  }
+  return(list(X = X, Y = Y, df=df))
+}
+
+DGP2 <- function(T_, N, beta=c(1,2)){
+  if(T_ < 2 || N < 2)
+    stop("T or N less than 2")
+  
+  alpha_i <- runif(N, min = 0, max = 1)
+  eps_t <- rnorm(T_, mean = 0, sd = 1)
+  z0 <- 0
+  z_t <- c(0.5*z0 + eps_t[1])
+  for(i in 2:T_)
+    z_t[i] <- 0.5*z_t[i-1] + eps_t[i]
+  v_i <- rnorm(N, mean = 0, sd = 2)
+  e_t <- rnorm(T_ + 1, mean = 0, sd = 1)
+  mu <- 5
+  w_t <- mu + e_t[2:(T_+1)] + e_t[1:T_] / 3
+  x0 = 1
+  df <- data.frame(i = rep(c(1:N), each = T_),
+                   t = rep(c(1:T_), times = N),
+                   x0 = x0,
+                   x_it = NA,
+                   u_it = NA,
+                   y_it = NA)
+  for(i in 1:N){
+    for(t in 1:T_){
+      df$x_it[(i-1)*T_ + t] <- sqrt(alpha_i[i]) * z_t[t]
+      df$u_it[(i-1)*T_ + t] <- v_i[i] + w_t[t]
+      df$y_it[(i-1)*T_ + t] <- alpha_i[i] + beta[1] * x0 +
+        beta[2] * df$x_it[(i-1)*T_ + t] + df$u_it[(i-1)*T_ + t]
+    }
+  }
+  return(df)
 }
 
 OLS_FE <- function(X, Y){
@@ -61,15 +103,24 @@ OLS_FE <- function(X, Y){
   return(list(beta_hat = beta_hat_fe))
 }
 
+OLS_FE2 <- function(df){
+  data <- pdata.frame(df,index=c("i","t"))
+  result <- plm(y_it ~ x0 + x_it, data=data,
+                effect = "twoways",model="within")
+  return(result$coefficients)
+}
+
+##### test #####
+df <- DGP2(5, 7)
+OLS_FE2(df)
+
 dgp = DGP(K = 3,
           T_ = 3,
           N = 400,
-          range_x = c(0,1),
-          beta_i = c(1,2,3),
-          alpha_i = 1,
-          mu_u_i = c(0,0,0),
-          sigma_u_i = diag(c(1,1,1)))
+          beta_i = c(1,2,3))
 OLS_FE(dgp$X, dgp$Y)
+plm(y_it ~ x1+x2+x3, data=dgp$df,
+    effect = "twoways",model="within")$coefficients
 
 
 ##### simulation #####
@@ -89,11 +140,7 @@ sim <- function(K, beta_true, all_n, all_T, nsims){
         dgp = DGP(K = K,
                   T_ =T_,
                   N = N,
-                  range_x = c(0,1),
-                  beta_i = beta_true,
-                  alpha_i = 1,
-                  mu_u_i = rep(0, T_),
-                  sigma_u_i = diag(rep(1, T_)))
+                  beta_i = beta_true)
         result = OLS_FE(dgp$X, dgp$Y)
         beta_hat = result$beta_hat
         beta_hat_1[i, j, h] = beta_hat[1]
